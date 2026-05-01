c8fb84e2a135c08224b88e02dc2ca9e125259b8f
Diana <diana.tanlabekova@narxoz.kz>
2026-05-01 23:22:08 +0500

    Implement Healer: healing/urgent colleague

--- src/com/narxoz/rpg/guild/Healer.java
+++ src/com/narxoz/rpg/guild/Healer.java
@@ -7,12 +7,16 @@ public class Healer extends GuildMember {
     }
 
     public void prepareAid(String topic, String payload) {
-        // TODO: send a healing message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        System.out.println("[Healer " + getName() + "] received on topic='" + topic + "' from " + from.getName() + ": " + payload);
+        if ("healing".equals(topic)) {
+            System.out.println("  -> Healer preparing potions and bandages for: " + payload);
+        } else if ("urgent".equals(topic)) {
+            System.out.println("  -> Healer rushing to emergency triage! " + payload);
+        }
     }
 }

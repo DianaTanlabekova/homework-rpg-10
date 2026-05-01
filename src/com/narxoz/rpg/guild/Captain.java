c41e66a9850c838e18e3fe61968ed9adb200fa08
Diana <diana.tanlabekova@narxoz.kz>
2026-05-02 01:39:12 +0500

    Implement Captain: orders/scouting/supplies/healing/urgent colleague

--- src/com/narxoz/rpg/guild/Captain.java
+++ src/com/narxoz/rpg/guild/Captain.java
@@ -1,21 +1,26 @@
-    package com.narxoz.rpg.guild;
+package com.narxoz.rpg.guild;
 
-    /**
-     * Guild officer responsible for orders and mission coordination.
-     */
-    public class Captain extends GuildMember {
+public class Captain extends GuildMember {
 
-        public Captain(String name, GuildMediator mediator) {
-            super(name, mediator);
-        }
+    public Captain(String name, GuildMediator mediator) {
+        super(name, mediator);
+    }
 
-        public void issueOrder(String topic, String payload) {
-            // TODO: send a command message through the mediator.
-            getMediator().dispatch(topic, this, payload);
-        }
+    public void issueOrder(String topic, String payload) {
+        getMediator().dispatch(topic, this, payload);
+    }
 
-        @Override
-        public void receive(String topic, GuildMember from, String payload) {
-            // TODO: react to a guild-hall message without calling another colleague directly.
+    @Override
+    public void receive(String topic, GuildMember from, String payload) {
+        System.out.println("[Captain " + getName() + "] received on topic='" + topic + "' from " + from.getName() + ": " + payload);
+        if ("scouting".equals(topic)) {
+            System.out.println("  -> Captain reviewing scout report and adjusting battle plan.");
+        } else if ("supplies".equals(topic)) {
+            System.out.println("  -> Captain approving supply allocation: " + payload);
+        } else if ("healing".equals(topic)) {
+            System.out.println("  -> Captain noting casualty report: " + payload);
+        } else if ("urgent".equals(topic)) {
+            System.out.println("  -> Captain sounding the alarm! " + payload);
         }
     }
+}
